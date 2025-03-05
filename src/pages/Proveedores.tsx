68d39c91bbb7ad9ff4b805794d8a1c515688e123
AntoniaCP <antoniaabigailcortesperez@gmail.com>
2025-03-05 11:16:37 -0600

    Modulo Proveeedores Consultar

--- src/pages/Proveedores.tsx
+++ src/pages/Proveedores.tsx
@@ -1,12 +1,21 @@
-// src/pages/Proveedores.tsx
-import React from 'react';
+import React, { useState } from 'react';
+import { Menu } from 'antd';
+import { UserAddOutlined, TableOutlined } from '@ant-design/icons'; 
 import ProveedorForm from '../components/ProveedorForm';
+import ProveedoresTable from '../components/ProveedoresTable';
 
 const Proveedores: React.FC = () => {
-  return (
-    <div style={{ padding: '20px' }}>
+  const [selectedTab, setSelectedTab] = useState('registrar');
 
-      <ProveedorForm />
+  return (
+    <div className="p-5">
+      <Menu mode="horizontal" className="mb-5 border-b" selectedKeys={[selectedTab]} onClick={(e) => setSelectedTab(e.key)}>
+        <Menu.Item key="registrar" icon={<UserAddOutlined />}>Registrar Proveedores</Menu.Item>
+        <Menu.Item key="gestionar" icon={<TableOutlined />}>Gestionar Proveedores</Menu.Item>
+      </Menu>
+
+      {selectedTab === 'registrar' && <ProveedorForm />}
+      {selectedTab === 'gestionar' && <ProveedoresTable />}
     </div>
   );
 };
